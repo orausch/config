a00458203379cc3e3d7b0c2ffa8ebc7d012509f3
Oliver Rausch <oliverrausch99@gmail.com>
2021-08-11 14:41:53 +0200

    Add nvim script

---
+++ bootstrap_nvim.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eux -o pipefail
+
+
+wget https://github.com/neovim/neovim/releases/download/v0.5.0/nvim-linux64.tar.gz
+mkdir -p ~/.local/share
+tar -xzf nvim-linux64.tar.gz --directory ~/.local/share
+mkdir -p ~/.local/bin
+ln -s -t ~/.local/bin ~/.local/share/nvim-linux64/bin/nvim
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
